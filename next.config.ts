232c51fa2750fde00e098cfcd1dc3b3b1dee5e9a
web728 <web@futureexpr.com>
2026-07-20 14:54:49 +0530

    sub domains added in config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "batteryindiaexpo.com",
       },
+      {
+        protocol: "https",
+        hostname: "info.batteryindiaexpo.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.batteryindiaexpo.com",
+      },
     ],
   },
 };
